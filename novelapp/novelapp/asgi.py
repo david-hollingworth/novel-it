9d886c6ad61fec74a1c16a049073f0469f040721
David Hollingworth <hollingworth.david@orange.fr>
2026-01-29 16:52:00 +0100

    Removed initial project structure and recreated. Created phase 1 Django apps.

--- novelapp/novelapp/asgi.py
+++ novelapp/novelapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for novelit project.
+ASGI config for novelapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'novelit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'novelapp.settings')
 
 application = get_asgi_application()
--- novelapp/novelapp/wsgi.py
+++ novelapp/novelapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for novelit project.
+WSGI config for novelapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'novelit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'novelapp.settings')
 
 application = get_wsgi_application()
